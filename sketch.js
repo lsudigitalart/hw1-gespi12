75f6d7607cae3db86c827cecb9b208f444455422
gespi12 <gespi12@lsu.edu>
2025-09-03 13:22:43 -0600

    commit

--- sketch.js
+++ sketch.js
@@ -3,6 +3,17 @@ function setup() {
 }
 
 function draw() {
-    background(200);
-    ellipse(200, 100, 25, 50);
+    background(0);
+    fill('white');
+    ellipse(200, 200, 450, 450);
+    fill('black');
+    
+    ellipse(200, 270, 25, 50);
+    ellipse(100, 225, 110, 110);
+    ellipse(300, 225, 110, 110);
+    fill('white');
+    ellipse(200,380,400,50);
+    fill('black');
+    ellipse(400, 375, 250, 205);
+    ellipse(0, 375, 250, 205);
 }
